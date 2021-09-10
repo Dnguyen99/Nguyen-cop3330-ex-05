968cde4f2d8bcad01927362205148280647902e8
Dnguyen99 <dylannguyen@Knights.ucf.edu>
2021-09-10 11:26:37 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is the first number? ");
+        int num1 = scan.nextInt();
+        System.out.println("What is the second number? ");
+        int num2 = scan.nextInt();
+
+        System.out.println(+num1+ " + " +num2+ " = " +(num1 + num2));
+        System.out.println(+num1+ " - " +num2+ " = " +(num1 - num2));
+        System.out.println(+num1+ " * " +num2+ " = " +(num1 * num2));
+        System.out.println(+num1+ " / " +num2+ " = " +(num1 / num2));
+
+    }
+}
